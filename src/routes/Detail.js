a21b15fee438a4e52a39c8d401e22be12fac00d1
SHan <hs04111@naver.com>
2021-06-19 23:16:43 +0900

    changing class to function using hooooooks

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,20 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-class Detail extends React.Component {
-  componentDidMount() {
-    const { location, history } = this.props;
+function Detail({ location, history }) {
+  useEffect(() => {
     if (location.state === undefined) {
       history.push("/");
     }
-  }
+  });
 
-  render() {
-    const { location } = this.props;
-    if (location.state) {
-      return <span>{location.state.title}</span>;
-    } else {
-      return null;
-    }
+  if (location.state) {
+    return <span>{location.state.title}</span>;
+  } else {
+    return null;
   }
 }
+
+// class Detail extends React.Component {
+//   componentDidMount() {
+//     const { location, history } = this.props;
+//     if (location.state === undefined) {
+//       history.push("/");
+//     }
+//   }
+
+//   render() {
+//     const { location } = this.props;
+//     if (location.state) {
+//       return <span>{location.state.title}</span>;
+//     } else {
+//       return null;
+//     }
+//   }
+// }
 export default Detail;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,55 +1,99 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Movie from "../components/Movie";
 import "./Home.css";
 
-class Home extends React.Component {
-  state = {
-    isLoading: true,
-    movies: [],
-  };
+function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
 
-  getMovies = async () => {
-    const {
-      data: {
-        data: { movies },
-      },
-    } = await axios.get(
-      "https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating"
-    );
-    this.setState({ movies, isLoading: false });
-  };
+  useEffect(() => {
+    async function fetchData() {
+      const {
+        data: {
+          data: { movies },
+        },
+      } = await axios.get(
+        "https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating"
+      );
+      setMovies(movies);
+      setIsLoading(false);
+    }
+    fetchData();
+  }, []);
 
-  componentDidMount() {
-    this.getMovies();
-  }
-
-  render() {
-    const { isLoading, movies } = this.state;
-    return (
-      <section className="container">
-        {isLoading ? (
-          <div className="loader">
-            <span className="loader__text">Loading...</span>
-          </div>
-        ) : (
-          <div className="movies">
-            {movies.map((movie) => (
-              <Movie
-                key={movie.id}
-                id={movie.id}
-                year={movie.year}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-                genres={movie.genres}
-              />
-            ))}
-          </div>
-        )}
-      </section>
-    );
-  }
+  return (
+    <section className="container">
+      {isLoading ? (
+        <div className="loader">
+          <span className="loader__text">Loading...</span>
+        </div>
+      ) : (
+        <div className="movies">
+          {movies.map((movie) => (
+            <Movie
+              key={movie.id}
+              id={movie.id}
+              year={movie.year}
+              title={movie.title}
+              summary={movie.summary}
+              poster={movie.medium_cover_image}
+              genres={movie.genres}
+            />
+          ))}
+        </div>
+      )}
+    </section>
+  );
 }
+
+// class Home extends React.Component {
+//   state = {
+//     isLoading: true,
+//     movies: [],
+//   };
+
+//   getMovies = async () => {
+//     const {
+//       data: {
+//         data: { movies },
+//       },
+//     } = await axios.get(
+//       "https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating"
+//     );
+//     this.setState({ movies, isLoading: false });
+//   };
+
+//   componentDidMount() {
+//     this.getMovies();
+//   }
+
+//   render() {
+//     const { isLoading, movies } = this.state;
+//     return (
+//       <section className="container">
+//         {isLoading ? (
+//           <div className="loader">
+//             <span className="loader__text">Loading...</span>
+//           </div>
+//         ) : (
+//           <div className="movies">
+//             {movies.map((movie) => (
+//               <Movie
+//                 key={movie.id}
+//                 id={movie.id}
+//                 year={movie.year}
+//                 title={movie.title}
+//                 summary={movie.summary}
+//                 poster={movie.medium_cover_image}
+//                 genres={movie.genres}
+//               />
+//             ))}
+//           </div>
+//         )}
+//       </section>
+//     );
+//   }
+// }
 
 export default Home;
